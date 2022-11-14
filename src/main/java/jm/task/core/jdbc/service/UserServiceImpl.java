7789c1bb86a8c925028ca02164dbce821212775e
AleksandrEzhov <104377724+AlexEzhov88@users.noreply.github.com>
2022-11-14 20:21:14 +0300

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,30 +1,27 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    private final UserDao userDaoHibernate = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
         userDaoHibernate.createUsersTable();
-        System.out.println("Таблица создана");
     }
 
     public void dropUsersTable() {
         userDaoHibernate.dropUsersTable();
-        System.out.println("Таблица удалена");
     }
 
     public void saveUser(String name, String lastName, byte age) {
         userDaoHibernate.saveUser(name, lastName, age);
-        System.out.println("User с именем – " + name + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
         userDaoHibernate.removeUserById(id);
-        System.out.println("User с id " + id + " удален");
     }
 
     public List<User> getAllUsers() {
@@ -33,6 +30,5 @@ public class UserServiceImpl implements UserService {
 
     public void cleanUsersTable() {
         userDaoHibernate.cleanUsersTable();
-        System.out.println("Таблица очищена");
     }
 }
